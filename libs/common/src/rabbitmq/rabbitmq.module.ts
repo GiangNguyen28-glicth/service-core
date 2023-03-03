94455e01891114776f31cfc31fe616271ba0a670
giangnt <giangnt@younetgroup.com>
2023-03-03 14:17:45 +0700

    handle connection rmq

--- libs/common/src/rabbitmq/rabbitmq.module.ts
+++ libs/common/src/rabbitmq/rabbitmq.module.ts
@@ -1,9 +1,56 @@
-import { Global, Module } from '@nestjs/common';
+import { DynamicModule, Global, Logger, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  ClientOptions,
+  ClientProxyFactory,
+  ClientsModule,
+  Transport,
+} from '@nestjs/microservices';
 import { RabbitService } from './rabbitmq.service';
-
+interface RmqModuleOptions {
+  name: string;
+}
 @Global()
 @Module({
   providers: [RabbitService],
   exports: [RabbitService],
 })
-export class RabbitModule {}
+export class RabbitModule {
+  static register({ name }: RmqModuleOptions): DynamicModule {
+    const logger = new Logger(RabbitModule.name);
+    return {
+      module: RabbitModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name,
+            useFactory: async (configService: ConfigService): Promise<any> => {
+              const options: ClientOptions = {
+                transport: Transport.RMQ,
+                options: {
+                  urls: ['amqp://guest:guest@localhost:56721'],
+                  queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+                },
+              };
+              logger.log(`Connecting to RMQ server...`);
+              const client = ClientProxyFactory.create(options);
+              const connectToRmq = async () => {
+                try {
+                  await client.connect();
+                  logger.log(`Connected to RMQ server`);
+                } catch (error) {
+                  logger.error(`Connect failed at ${new Date()}`);
+                  setTimeout(connectToRmq.bind(this), 3000);
+                }
+              };
+              await connectToRmq();
+              return client;
+            },
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      exports: [ClientsModule],
+    };
+  }
+}
